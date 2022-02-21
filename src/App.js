32253bdb999026e7637b2823d493b6e672c048db
Aniket Kariya <aniket.kariya@gmail.com>
2022-02-21 16:00:29 +0530

    added auth

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Amplify } from "aws-amplify";
+
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from v2</h1>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user}) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign Out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
 
